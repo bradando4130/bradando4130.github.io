64b0344c34525e4b33fa0eac0ec3b1bbac7cbfde
bradando4130 <bradando4130@gmail.com>
2022-06-23 20:05:01 +1000

    1.1.3 post release
    - custom form validation with JavaScript on contact me form
    - minor text colour changes and hover state for links
    - minor HTML adjustments on tags to fix oversights

--- js/app.js
+++ js/app.js
@@ -126,7 +126,7 @@ function buildModal(currentModalInfo) {
         }
 
 
-
+/* Nav functionality */
 // hamburger nav function to toggle to cross and display nav menu
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle('change');
@@ -149,3 +149,164 @@ window.addEventListener('resize', () => {
     navList.classList.add('hidden');
   }
 })
+
+
+/* Form Validation */
+// re-used variables
+let nameInput = document.getElementById('name');
+let emailInput = document.getElementById('email');
+let messageInput = document.getElementById('message');
+let myForm = document.getElementById('myForm');
+
+// clear inputs on page reload
+window.onunload = function () {
+	nameInput.value = '';
+  messageInput.value = '';
+  emailInput.value = '';
+}
+
+// custom submit - running validations -  and prevent submit on fail
+myForm.addEventListener('submit', function (e) {
+
+  // validate form inputs
+  let isUsernameValid = checkName(),
+      isEmailValid = checkEmail(),
+      isMessageValid = checkMessage();
+
+  // check if all valid
+  let isFormValid = isUsernameValid &&
+      isEmailValid &&
+      isMessageValid;
+
+  // submit if all inputs valid
+  if (isFormValid) {
+    
+  } else {
+    // prevent submit
+    e.preventDefault();
+  }
+});
+
+// check if email valid using regular expression
+const validateEmail = (email) => { 
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+// function to validate if field is empty
+const isEmpty = value => value === '' ? true : false;
+
+// function to validate if value is between two lengths
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+// show error function to highlight input red and display error message
+const showError = (input, message) => {
+  const formPair = input.parentElement;
+  // add error class and remove sucess
+  formPair.classList.remove('input-sucess');
+  formPair.classList.add('input-error');
+  // display error message to user
+  const error = formPair.querySelector('small');
+  error.textContent = message;
+};
+
+// show sucess function to highlight input green
+const showSuccess = (input) => {
+  const formPair = input.parentElement;
+  // remove error class and add sucess
+  formPair.classList.remove('input-error');
+  formPair.classList.add('input-sucess');
+  // hide error message
+  const error = formPair.querySelector('small');
+  error.textContent = '';
+}
+
+// function to check if username is valid
+const checkName = () => {
+
+  // default false
+  let valid = false;
+  const min = 3,
+        max = 25;
+  const userNameTrim = nameInput.value.trim();
+
+  // if username is blank
+  if (isEmpty(userNameTrim)) {
+    showError(nameInput, 'Username cannot be empty.');
+  // if username is bwtween desired length  
+  } else if (!isBetween(userNameTrim.length, min, max)) {
+    showError(nameInput, `Username must be between ${min} and ${max} characters.`)
+  } else {
+    showSuccess(nameInput);
+    valid = true;
+  }
+  return valid;
+}
+
+// function to check email is valid
+const checkEmail = () => {
+  
+  // default false
+  let valid = false;
+  const emailTrim = emailInput.value.trim();
+
+  if (isEmpty(emailTrim)) {
+    showError(emailInput, 'Email cannot be empty.')
+  } else if (!validateEmail(emailTrim)) {
+    showError(emailInput, 'Email is not valid.')
+  } else {
+    showSuccess(emailInput);
+    valid = true;
+  }
+  return valid;
+}
+
+// function to check is message valid
+const checkMessage = () => {
+
+  // default false
+  let valid = false;
+  const messageTrim = messageInput.value.trim();
+  
+  // if message blank return invalid
+  if (isEmpty(messageTrim)) {
+    showError(messageInput, 'Please leave a message.')
+  } else {
+    showSuccess(messageInput)
+    valid = true;
+  }
+  return valid;
+}
+
+// debounce function for ~0.5s delay on form validation check
+const debounce = (fn, delay = 800) => {
+  let timeoutId;
+  return (...args) => {
+    // cancel the previous timer
+    if (timeoutId) {
+      clearTimeout(timer);
+    }
+    // set new timer
+    timer = setTimeout(() => {
+      fn.apply(null, args) 
+    }, delay);
+  };
+};
+
+// instant feedback event listener on form
+myForm.addEventListener('input', debounce(function (e) {
+  switch (e.target.id) {
+    case 'name': 
+      checkName();
+      break;
+    case 'email':
+      checkEmail();
+      break;
+    case 'message':
+      checkMessage();
+      break;
+  }
+}));
+
+
+
